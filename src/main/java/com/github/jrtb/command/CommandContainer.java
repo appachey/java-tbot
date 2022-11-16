1294ca7e598c4cb5ace6707c02df339f057fa7c0
sergey <appachey@gmail.com>
2022-11-16 12:49:33 +0200

    JRTB-7: added ability to delete group subscription.

--- src/main/java/com/github/jrtb/command/CommandContainer.java
+++ src/main/java/com/github/jrtb/command/CommandContainer.java
@@ -24,7 +24,10 @@ public class CommandContainer {
                 .put(HELP.getCommandName(), new HelpCommand(sendBotMessageService))
                 .put(NO.getCommandName(), new NoCommand(sendBotMessageService))
                 .put(STAT.getCommandName(), new StatCommand(sendBotMessageService, telegramUserService))
-                .put(ADD_GROUP_SUB.getCommandName(), new AddGroupSubCommand(sendBotMessageService, javaRushGroupClient, groupSubService))
+                .put(ADD_GROUP_SUB.getCommandName(), new AddGroupSubCommand(sendBotMessageService,
+                        javaRushGroupClient, groupSubService))
+                .put(DELETE_GROUP_SUB.getCommandName(), new DeleteGroupSubCommand(sendBotMessageService,
+                        groupSubService, telegramUserService))
                 .put(LIST_GROUP_SUB.getCommandName(), new ListGroupSubCommand(sendBotMessageService, telegramUserService))
                 .build();
         unknownCommand = new UnknownCommand(sendBotMessageService);
--- src/main/java/com/github/jrtb/service/GroupSubService.java
+++ src/main/java/com/github/jrtb/service/GroupSubService.java
@@ -3,10 +3,14 @@ package com.github.jrtb.service;
 import com.github.jrtb.javarushclient.dto.GroupDiscussionInfo;
 import com.github.jrtb.repository.entity.GroupSub;
 
+import java.util.Optional;
+
 /**
  * Service for manipulating with {@link GroupSub}
  */
 public interface GroupSubService {
 
     GroupSub save(Long chatId, GroupDiscussionInfo groupDiscussionInfo);
+    GroupSub save(GroupSub groupSub);
+    Optional<GroupSub> findById(Integer id);
 }
