3b96b6afd64d29b11a4678657e9b4de2c6bae7e9
appachey <31824820+appachey@users.noreply.github.com>
2022-11-21 23:28:39 +0200

    STEP_8: added ability to find new articles and send notifications about new of them. (#27)

--- src/main/java/com/github/jrtb/command/StopCommand.java
+++ src/main/java/com/github/jrtb/command/StopCommand.java
@@ -21,11 +21,11 @@ public class StopCommand implements Command {
 
     @Override
     public void execute(Update update) {
-        sendBotMessageService.sendMessage(update.getMessage().getChatId(), STOP_MESSAGE);
         telegramUserService.findByChatId(update.getMessage().getChatId())
                 .ifPresent(it -> {
                     it.setActive(false);
                     telegramUserService.save(it);
                 });
+        sendBotMessageService.sendMessage(update.getMessage().getChatId(), STOP_MESSAGE);
     }
 }
