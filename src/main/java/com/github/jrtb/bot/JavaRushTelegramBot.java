beede76d0a87c2bc14afb4850e12e63f2a5158e8
ThinkPad <appachey@gmail.com>
2022-12-11 23:31:54 +0200

    STEP_9_JRTB-10: added AdminCommand annotation and AdminHelpCommand.

--- src/main/java/com/github/jrtb/bot/JavaRushTelegramBot.java
+++ src/main/java/com/github/jrtb/bot/JavaRushTelegramBot.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.util.List;
+
 import static com.github.jrtb.command.CommandName.*;
 
 @Component
@@ -28,8 +30,9 @@ public class JavaRushTelegramBot extends TelegramLongPollingBot {
 
     @Autowired
     public JavaRushTelegramBot(TelegramUserService telegramUserService, JavaRushGroupClient groupClient,
-                               GroupSubService groupSubService) {
-        this.commandContainer = new CommandContainer(new SendBotMessageServiceImpl(this), telegramUserService, groupClient, groupSubService);
+                               GroupSubService groupSubService, @Value("#{'${bot.admins}'.split(',')}") List<String> admins) {
+        this.commandContainer = new CommandContainer(new SendBotMessageServiceImpl(this), telegramUserService,
+                groupClient, groupSubService, admins);
     }
 
     @Override
@@ -41,12 +44,13 @@ public class JavaRushTelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String message = update.getMessage().getText().trim();
+            String username = update.getMessage().getFrom().getUserName();
             if (message.startsWith(COMMAND_PREFIX)) {
                 String commandIdentifier = message.split(" ")[0].toLowerCase();
 
-                commandContainer.retrieveCommand(commandIdentifier).execute(update);
+                commandContainer.retrieveCommand(commandIdentifier, username).execute(update);
             } else {
-                commandContainer.retrieveCommand(NO.getCommandName()).execute(update);
+                commandContainer.retrieveCommand(NO.getCommandName(), username).execute(update);
             }
         }
 
